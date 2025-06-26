c3f1f4da0d8c09ebbf7d42a46b4f7864cca3767a
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2025-06-27 02:30:13 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/27/2025, 2:30:11 AM
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while(n != 0) {
+            if(n & 1 ){
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+};
